56ba87f178d85d0035d18243b8b88415e4c88bee
minminmail@hotmail.com <minminmail@hotmail.com>
2019-08-11 01:54:56 +0800

    format files

--- main/Rule.py
+++ main/Rule.py
@@ -1,4 +1,3 @@
-
 # /***********************************************************************
 #
 # 	This file is part of KEEL-software, the Data Mining tool for regression,
@@ -32,191 +31,182 @@ import Fuzzy_Chi
 from Fuzzy import Fuzzy
 
 
- # * <p>This class contains the structure of a Fuzzy Rule</p>
- # *
- # * @author Written by Alberto Fern谩ndez (University of Granada) 29/10/2007
- # * @version 1.0
- # * @since JDK1.5
+# * <p>This class contains the structure of a Fuzzy Rule</p>
+# *
+# * @author Written by Alberto Fern谩ndez (University of Granada) 29/10/2007
+# * @version 1.0
+# * @since JDK1.5
 
 class Rule:
+    antecedent = None
+    class_value = None
+    weight = None
+    compatibilityType = None
 
-  antecedent=None
-  clas=None
-  weight=None
-  compatibilityType=None
+    def __init__(self):
+        print("__init__ of Rule")
 
-  def __init__(self):
-    print("__init__ of Rule")
+    # Default constructor
 
-  #Default constructor
+    # * Constructor with parameters
+    # * @param n_variables int
+    # * @param compatibilityType int
 
-     # * Constructor with parameters
-     # * @param n_variables int
-     # * @param compatibilityType int
+    def setTwoParameters(self, n_variables, compatibilityType):
+        print("In rule calss , setTwoParameters method, the n_variables = " + str(n_variables))
+        self.antecedent = [Fuzzy() for x in range(n_variables)]
+        self.compatibilityType = compatibilityType
 
-  def setTwoParameters( self,n_variables,  compatibilityType):
-    print("In rule calss , setTwoParameters method, the n_variables = "+str(n_variables))
-    self.antecedent = [Fuzzy() for x in range(n_variables)]
-    self.compatibilityType = compatibilityType
+        # * It assigns the class of the rule
+        # * @param clas int
 
-     # * It assigns the class of the rule
-     # * @param clas int
+    def setClass(self, clas):
+        self.class_value = clas
 
-  def setClass(self, clas):
-    self.clas = clas
+    # * It assigns the rule weight to the rule
+    # * @param train myDataset the training set
+    # * @param ruleWeight int the type of rule weight
 
-   # * It assigns the rule weight to the rule
-   # * @param train myDataset the training set
-   # * @param ruleWeight int the type of rule weight
+    def assingConsequent(self, train, ruleWeight):
+        if ruleWeight == Fuzzy_Chi.Fuzzy_Chi.CF:
+            self.consequent_CF(train)
 
-  def assingConsequent(self,train, ruleWeight) :
-    if ruleWeight == Fuzzy_Chi.Fuzzy_Chi.CF:
-      self.consequent_CF(train)
+        elif ruleWeight == Fuzzy_Chi.Fuzzy_Chi.PCF_II:
+            self.consequent_PCF2(train)
 
-    elif ruleWeight == Fuzzy_Chi.Fuzzy_Chi.PCF_II:
-      self.consequent_PCF2(train)
+        elif ruleWeight == Fuzzy_Chi.Fuzzy_Chi.PCF_IV:
+            self.consequent_PCF4(train)
 
-    elif ruleWeight == Fuzzy_Chi.Fuzzy_Chi.PCF_IV:
-      self.consequent_PCF4(train)
+        elif ruleWeight == Fuzzy_Chi.Fuzzy_Chi.NO_RW:
+            self.weight = 1.0
 
-    elif ruleWeight == Fuzzy_Chi.Fuzzy_Chi.NO_RW:
-      self.weight = 1.0
+    # * It computes the compatibility of the rule with an input example
+    # * @param example double[] The input example
+    # * @return double the degree of compatibility
 
-   # * It computes the compatibility of the rule with an input example
-   # * @param example double[] The input example
-   # * @return double the degree of compatibility
+    def compatibility(self, example):
+        if self.compatibilityType == Fuzzy_Chi.Fuzzy_Chi.MINIMUM:
+            # print("self.compatibilityType == Fuzzy_Chi.Fuzzy_Chi.MINIMUM")
+            return self.minimumCompatibility(example)
 
-  def compatibility(self,example):
-    if (self.compatibilityType == Fuzzy_Chi.Fuzzy_Chi.MINIMUM):
-      #print("self.compatibilityType == Fuzzy_Chi.Fuzzy_Chi.MINIMUM")
-      return self.minimumCompatibility(example)
+        else:
+            # print("self.compatibilityType != Fuzzy_Chi.Fuzzy_Chi.MINIMUM"+", self.compatibilityType = "+ str(
+            # self.compatibilityType))
+            return self.productCompatibility(example)
 
-    else :
-      #print("self.compatibilityType != Fuzzy_Chi.Fuzzy_Chi.MINIMUM"+", self.compatibilityType = "+ str(self.compatibilityType))
-      return self.productCompatibility(example)
+    # * Operator T-min
+    # * @param example double[] The input example
+    # * @return double the computation the the minimum T-norm
 
+    def minimumCompatibility(self, example):
+        minimum = None
+        membershipDegree = None
+        minimum = 1.0
+        for i in range(0, len(self.antecedent)):
+            print("example[" + str(i) + "] = " + example[i])
+            membershipDegree = self.antecedent[i].setX(example[i])
+            print("membershipDegree in minimumCompatibility = " + str(membershipDegree))
+            minimum = min(membershipDegree, minimum)
 
-   # * Operator T-min
-   # * @param example double[] The input example
-   # * @return double the computation the the minimum T-norm
+        return minimum
 
-  def minimumCompatibility(self,example):
-    minimum=None
-    membershipDegree=None
-    minimum = 1.0
-    for i in range(0, len(self.antecedent)):
-      print("example["+str(i)+"] = "+example[i])
-      membershipDegree = self.antecedent[i].setX(example[i])
-      print("membershipDegree in minimumCompatibility = " + str(membershipDegree))
-      minimum = min(membershipDegree, minimum)
+    # * Operator T-product
+    # * @param example double[] The input example
+    # * @return double the computation the the product T-norm
 
-    return minimum
+    def productCompatibility(self, example):
 
-   # * Operator T-product
-   # * @param example double[] The input example
-   # * @return double the computation the the product T-norm
+        product = 1.0
+        antecedent_number = len(self.antecedent)
+        # print("antecedent_number = " + str(antecedent_number))
+        for i in range(0, antecedent_number):
+            # print("example[i="+ str(i)+"]"+":"+ str(example[i]))
+            membershipDegree = self.antecedent[i].setX(example[i])
+            # print("membershipDegree in productCompatibility  = " +str(membershipDegree))
+            product = product * membershipDegree
+        # print("product: "+ str(product))
+        return product
 
-  def productCompatibility(self, example):
+    # * Classic Certainty Factor weight
+    # * @param train myDataset training dataset
 
-    product = 1.0
-    antecedent_number=len(self.antecedent)
-    #print("antecedent_number = " + str(antecedent_number))
-    for i in range( 0, antecedent_number):
-      #print("example[i="+ str(i)+"]"+":"+ str(example[i]))
-      membershipDegree = self.antecedent[i].setX(example[i])
-      #print("membershipDegree in productCompatibility  = " +str(membershipDegree))
-      product = product * membershipDegree
-    #print("product: "+ str(product))
-    return product
+    def consequent_CF(self, train):
+        train_Class_Number = train.getnClasses()
+        classes_sum = [0.0 for x in range(train_Class_Number)]
+        for i in range(0, train.getnClasses()):
+            classes_sum[i] = 0.0
 
+        total = 0.0
+        comp = None
+        # Computation of the sum by classes */
+        for i in range(0, train.size()):
+            comp = self.compatibility(train.getExample(i))
+            classes_sum[train.getOutputAsIntegerWithPos(i)] = classes_sum[train.getOutputAsIntegerWithPos(i)] + comp
+            total = total + comp
 
-   # * Classic Certainty Factor weight
-   # * @param train myDataset training dataset
+        print("classes_sum[self.clas]  = " + str(classes_sum[self.class_value]) + "total" + str(total))
+        self.weight = classes_sum[self.class_value] / total
 
-  def consequent_CF( self,train):
-    train_Class_Number = train.getnClasses()
-    classes_sum = [0.0 for x in range(train_Class_Number)]
-    for i in range( 0,train.getnClasses()):
-       classes_sum[i] = 0.0
+    # * Penalized Certainty Factor weight II (by Ishibuchi)
+    # * @param train myDataset training dataset
 
-    total = 0.0
-    comp = None
-    #Computation of the sum by classes */
-    for i in range( 0,train.size()):
-      comp = self.compatibility(train.getExample(i))
-      classes_sum[train.getOutputAsIntegerWithPos(i)] = classes_sum[train.getOutputAsIntegerWithPos(i)]+ comp
-      total =total+ comp
+    def consequent_PCF2(self, train):
+        classes_sum = float[train.getnClasses()]
+        for i in range(0, train.getnClasses()):
+            classes_sum[i] = 0.0
 
-    print("classes_sum[self.clas]  = " + str(classes_sum[self.clas] ) +"total" +str(total))
-    self.weight = classes_sum[self.clas] / total
+        total = 0.0
+        comp = None
+        # Computation of the sum by classes */
+        for i in range(0, train.size()):
+            comp = self.compatibility(train.getExample(i))
+            classes_sum[train.getOutputAsIntegerWithPos(i)] = classes_sum[train.getOutputAsIntegerWithPos(i)] + comp
+            total = total + comp
 
-   # * Penalized Certainty Factor weight II (by Ishibuchi)
-   # * @param train myDataset training dataset
+        sum = (total - classes_sum[self.class_value]) / (train.getnClasses() - 1.0)
+        self.weight = (classes_sum[self.class_value] - sum) / total
 
-  def consequent_PCF2(self,train) :
-    classes_sum = float[train.getnClasses()]
-    for i in range (0, train.getnClasses()):
-       classes_sum[i] = 0.0
+    # * Penalized Certainty Factor weight IV (by Ishibuchi)
+    # * @param train myDataset training dataset
 
-    total = 0.0
-    comp = None
-  # Computation of the sum by classes */
-    for i in range (0,  train.size()):
-      comp = self.compatibility(train.getExample(i))
-      classes_sum[train.getOutputAsIntegerWithPos(i)] = classes_sum[train.getOutputAsIntegerWithPos(i)]+comp
-      total = total+comp
+    def consequent_PCF4(self, train):
+        classes_sum = [0.0 for x in range(train.getnClasses())]
+        for i in range(0, train.getnClasses()):
+            classes_sum[i] = 0.0
 
-    sum = (total - classes_sum[self.clas]) / (train.getnClasses() - 1.0)
-    self.weight = (classes_sum[self.clas] - sum) / total
+        total = 0.0
+        comp = None
 
-   # * Penalized Certainty Factor weight IV (by Ishibuchi)
-   # * @param train myDataset training dataset
+        train_size = train.size()
+        print("train_size: " + str(train_size))
+        # Computation of the sum by classes */
+        for i in range(0, train_size):
+            comp = self.compatibility(train.getExample(i))
+            print("comp = " + str(comp))
+            classes_sum[train.getOutputAsIntegerWithPos(i)] = classes_sum[train.getOutputAsIntegerWithPos(i)] + comp
+            total = total + comp
 
-  def consequent_PCF4( self,train) :
-    classes_sum =  [0.0 for x in range(train.getnClasses())]
-    for  i in range( 0, train.getnClasses()):
-      classes_sum[i] = 0.0
+        print("self.clas =" + str(self.class_value) + "classes_sum[self.clas] :" + str(classes_sum[self.class_value]))
+        sum_value = total - classes_sum[self.class_value]
+        self.weight = (classes_sum[self.class_value] - sum_value) / total
 
-    total = 0.0
-    comp= None
+    # * This function detects if one rule is already included in the Rule Set
+    # * @param r Rule Rule to compare
+    # * @return boolean true if the rule already exists, else false
 
-    train_size=train.size()
-    print("train_size: " + str(train_size))
-    # Computation of the sum by classes */
-    for i in range( 0, train_size):
-      comp = self.compatibility(train.getExample(i))
-      print("comp = " + str(comp))
-      classes_sum[train.getOutputAsIntegerWithPos(i)] = classes_sum[train.getOutputAsIntegerWithPos(i)]+ comp
-      total = total+ comp
+    def comparison(self, rule):
+        contador_value = 0
+        for j in range(0, len(self.antecedent)):
+            if self.antecedent[j].label == rule.antecedent[j].label:
+                contador_value = contador_value + 1
 
-    print("self.clas ="+ str(self.clas)+"classes_sum[self.clas] :" + str(classes_sum[self.clas]))
-    sum = total - classes_sum[self.clas]
-    self.weight = (classes_sum[self.clas] - sum) / total
+        if contador_value == len(rule.antecedent):
+            if self.class_value != rule.clas:  # Comparison of the rule weights
+                if self.weight < rule.weight:
+                    # Rule Update
+                    self.class_value = rule.clas
+                    self.weight = rule.weight
 
-   # * This function detects if one rule is already included in the Rule Set
-   # * @param r Rule Rule to compare
-   # * @return boolean true if the rule already exists, else false
-
-  def comparison(self,rule) :
-    contador = 0
-    for j in range (0, len(self.antecedent)):
-      if (self.antecedent[j].label == rule.antecedent[j].label) :
-        contador= contador + 1
-
-    if (contador == len(rule.antecedent)):
-      if (self.clas != rule.clas) : #Comparison of the rule weights
-        if (self.weight < rule.weight) :
-          #Rule Update
-          self.clas = rule.clas
-          self.weight = rule.weight
-
-      return True
-    else:
-      return False
-
-
-
-
-
-
-
+            return True
+        else:
+            return False
--- main/RuleBase.py
+++ main/RuleBase.py
@@ -1,4 +1,3 @@
-
 # /***********************************************************************
 #
 # 	This file is part of KEEL-software, the Data Mining tool for regression,
@@ -32,186 +31,183 @@ from DataBase import DataBase
 from Rule import Rule
 import Fuzzy_Chi
 
- # * This class contains the representation of a Rule Set
- # *
- # * @author Written by Alberto Fern谩ndez (University of Granada) 29/10/2007
- # * @version 1.0
- # * @since JDK1.5
 
-class RuleBase :
+# * This class contains the representation of a Rule Set
+# *
+# * @author Written by Alberto Fern谩ndez (University of Granada) 29/10/2007
+# * @version 1.0
+# * @since JDK1.5
 
-    ruleBase=[]
-    dataBase=DataBase()
-    n_variables= None
-    n_labels= None
-    ruleWeight= None
-    inferenceType= None
-    compatibilityType= None
-    names=[]
-    classes=[]
+class RuleBase:
+    ruleBase = []
+    dataBase = DataBase()
+    n_variables = None
+    n_labels = None
+    ruleWeight = None
+    inferenceType = None
+    compatibilityType = None
+    names = []
+    classes = []
 
-        # /**
-        #  * Rule Base Constructor
-        #  * @param dataBase DataBase the Data Base containing the fuzzy partitions
-        #  * @param inferenceType int the inference type for the FRM
-        #  * @param compatibilityType int the compatibility type for the t-norm
-        #  * @param ruleWeight int the rule weight heuristic
-        #  * @param names String[] the names for the features of the problem
-        #  * @param classes String[] the labels for the class attributes
-        #  */
+    # /**
+    #  * Rule Base Constructor
+    #  * @param dataBase DataBase the Data Base containing the fuzzy partitions
+    #  * @param inferenceType int the inference type for the FRM
+    #  * @param compatibilityType int the compatibility type for the t-norm
+    #  * @param ruleWeight int the rule weight heuristic
+    #  * @param names String[] the names for the features of the problem
+    #  * @param classes String[] the labels for the class attributes
+    #  */
 
-    def __init__(self, dataBase,  inferenceType,  compatibilityType, ruleWeight, names,  classes):
-            print("RuleBase init begin...")
-            self.ruleBase = []
-            self.dataBase = dataBase
-            self.n_variables = dataBase.numVariables()
-            self.n_labels = dataBase.numLabels()
-            self.inferenceType = inferenceType
-            self.compatibilityType = compatibilityType
-            self.ruleWeight = ruleWeight
-            self.names = names
-            self.classes = classes
+    def __init__(self, dataBase, inferenceType, compatibilityType, ruleWeight, names, classes):
+        print("RuleBase init begin...")
+        self.ruleBase = []
+        self.dataBase = dataBase
+        self.n_variables = dataBase.numVariables()
+        self.n_labels = dataBase.numLabels()
+        self.inferenceType = inferenceType
+        self.compatibilityType = compatibilityType
+        self.ruleWeight = ruleWeight
+        self.names = names
+        self.classes = classes
 
-         # * It checks if a specific rule is already in the rule base
-         # * @param r Rule the rule for comparison
-         # * @return boolean true if the rule is already in the rule base, false in other case
+    # * It checks if a specific rule is already in the rule base
+    # * @param r Rule the rule for comparison
+    # * @return boolean true if the rule is already in the rule base, false in other case
 
-    def duplicated(self,rule):
-            i = 0
-            found = False
-            while ((i < len(self.ruleBase)) and (not found)):
-                found = self.ruleBase[i].comparison(rule)
-                i+=1
-            return found
+    def duplicated(self, rule):
+        i = 0
+        found = False
+        while (i < len(self.ruleBase)) and (not found):
+            found = self.ruleBase[i].comparison(rule)
+            i = i + 1
+        return found
 
-         # * Rule Learning Mechanism for the Chi et al.'s method
-         # * @param train myDataset the training data-set
+    # * Rule Learning Mechanism for the Chi et al.'s method
+    # * @param train myDataset the training data-set
 
-    def Generation( self,train) :
-            print("In Generation, the size of train is :" +str(train.size()))
-            for i in range( 0, train.size()) :
-                rule = self.searchForBestAntecedent(train.getExample(i),train.getOutputAsIntegerWithPos(i))
-                rule.assingConsequent(train, self.ruleWeight)
-                if (not (self.duplicated(rule)) and(rule.weight > 0)):
-                    self.ruleBase.append(rule)
-         # * This function obtains the best fuzzy label for each variable of the example and assigns
-         # * it to the rule
-         # * @param example double[] the input example
-         # * @param clas int the class of the input example
-         # * @return Rule the fuzzy rule with the highest membership degree with the example
+    def Generation(self, train):
+        print("In Generation, the size of train is :" + str(train.size()))
+        for i in range(0, train.size()):
+            rule = self.searchForBestAntecedent(train.getExample(i), train.getOutputAsIntegerWithPos(i))
+            rule.assingConsequent(train, self.ruleWeight)
+            if not (self.duplicated(rule)) and (rule.weight > 0):
+                self.ruleBase.append(rule)
 
-    def searchForBestAntecedent(self,example,clas):
-            ruleInstance=Rule( )
-            ruleInstance.setTwoParameters(self.n_variables, self.compatibilityType)
-            print("In searchForBestAntecedent ,self.n_variables is :" + str(self.n_variables))
-            ruleInstance.setClass(clas)
-            print("In searchForBestAntecedent ,self.n_labels is :" + str(self.n_labels))
-            for i in range( 0,self.n_variables):
-                max = 0.0
-                etq = -1
-                per= None
-                for j in range( 0, self.n_labels) :
-                    print("Inside the second loop of searchForBestAntecedent......")
-                    per = self.dataBase.membershipFunction(i, j, example[i])
-                    if (per > max) :
-                        max = per
-                        etq = j
-                if (max == 0.0) :
-                    print("There was an Error while searching for the antecedent of the rule")
-                    print("Example: ")
-                    for j in range(0,self.n_variables):
-                        print(example[j] + "\t")
+    # * This function obtains the best fuzzy label for each variable of the example and assigns
+    # * it to the rule
+    # * @param example double[] the input example
+    # * @param clas int the class of the input example
+    # * @return Rule the fuzzy rule with the highest membership degree with the example
 
-                    print("Variable " + str(i))
-                    exit(1)
+    def searchForBestAntecedent(self, example, clas):
+        ruleInstance = Rule()
+        ruleInstance.setTwoParameters(self.n_variables, self.compatibilityType)
+        print("In searchForBestAntecedent ,self.n_variables is :" + str(self.n_variables))
+        ruleInstance.setClass(clas)
+        print("In searchForBestAntecedent ,self.n_labels is :" + str(self.n_labels))
+        for i in range(0, self.n_variables):
+            max_value = 0.0
+            etq = -1
+            per = None
+            for j in range(0, self.n_labels):
+                print("Inside the second loop of searchForBestAntecedent......")
+                per = self.dataBase.membershipFunction(i, j, example[i])
+                if per > max_value:
+                    max_value = per
+                    etq = j
+            if max_value == 0.0:
+                print("There was an Error while searching for the antecedent of the rule")
+                print("Example: ")
+                for j in range(0, self.n_variables):
+                    print(example[j] + "\t")
 
-                ruleInstance.antecedent[i] = self.dataBase.clone(i, etq)
-            return ruleInstance
-         # * It prints the rule base into an string
-         # * @return String an string containing the rule base
+                print("Variable " + str(i))
+                exit(1)
 
-    def printString(self) :
-            i=None
-            j= None
-            cadena = ""
-            cadena += "@Number of rules: " + str(len(self.ruleBase)) + "\n\n"
-            for i in range( 0, len(self.ruleBase)):
-                rule = self.ruleBase[i]
-                cadena += str(i + 1) + ": "
-                for j in range(0,  self.n_variables - 1) :
-                    cadena += self.names[j] + " IS " + rule.antecedent[j].name + " AND "
-                j=j+1
-                cadena += self.names[j] + " IS " + rule.antecedent[j].name + ": " + str(self.classes[rule.clas]) + " with Rule Weight: " + str(rule.weight) + "\n"
-            print("RuleBase cadena is:" + cadena)
-            return cadena
+            ruleInstance.antecedent[i] = self.dataBase.clone(i, etq)
+        return ruleInstance
 
-         # * It writes the rule base into an ouput file
-         # * @param filename String the name of the output file
+    # * It prints the rule base into an string
+    # * @return String an string containing the rule base
 
-    def writeFile(self,filename) :
-            outputString = ""
-            outputString = self.printString()
-            file = open(filename, "w")
-            file.write(outputString)
-            file.close()
-         # * Fuzzy Reasoning Method
-         # * @param example double[] the input example
-         # * @return int the predicted class label (id)
+    def printString(self):
+        i = None
+        j = None
+        cadena_string = ""
+        cadena_string += "@Number of rules: " + str(len(self.ruleBase)) + "\n\n"
+        for i in range(0, len(self.ruleBase)):
+            rule = self.ruleBase[i]
+            cadena_string += str(i + 1) + ": "
+            for j in range(0, self.n_variables - 1):
+                cadena_string += self.names[j] + " IS " + rule.antecedent[j].name + " AND "
+            j = j + 1
+            cadena_string += self.names[j] + " IS " + rule.antecedent[j].name + ": " + str(
+                self.classes[rule.clas]) + " with Rule Weight: " + str(rule.weight) + "\n"
+        print("RuleBase cadena_string is:" + cadena_string)
+        return cadena_string
 
-    def FRM(self,example):
-          if (self.inferenceType == Fuzzy_Chi.Fuzzy_Chi.WINNING_RULE):
-                return self.FRM_WR(example)
-          else :
-                return self.FRM_AC(example)
+    # * It writes the rule base into an ouput file
+    # * @param filename String the name of the output file
 
-         # * Winning Rule FRM
-         # * @param example double[] the input example
-         # * @return int the class label for the rule with highest membership degree to the example
-    def FRM_WR(self,example):
-            clas = -1
-            max = 0.0
-            for i in range( 0, len(self.ruleBase)):
-                rule= self.ruleBase[i]
-                produc = rule.compatibility(example)
-                produc *= rule.weight
-                if (produc > max) :
-                    max = produc
-                    clas = rule.clas
-            return clas
+    def writeFile(self, filename):
+        outputString = ""
+        outputString = self.printString()
+        file = open(filename, "w")
+        file.write(outputString)
+        file.close()
 
-     # * Additive Combination FRM
-     # * @param example double[] the input example
-     # * @return int the class label for the set of rules with the highest sum of membership degree per class
+    # * Fuzzy Reasoning Method
+    # * @param example double[] the input example
+    # * @return int the predicted class label (id)
 
-    def FRM_AC(self,example):
-         clas = -1
-         class_degrees = []
-         for i in range( 0, len(self.ruleBase)) :
+    def FRM(self, example):
+        if self.inferenceType == Fuzzy_Chi.Fuzzy_Chi.WINNING_RULE:
+            return self.FRM_WR(example)
+        else:
+            return self.FRM_AC(example)
+
+    # * Winning Rule FRM
+    # * @param example double[] the input example
+    # * @return int the class label for the rule with highest membership degree to the example
+    def FRM_WR(self, example):
+        class_value = -1
+        max_value = 0.0
+        for i in range(0, len(self.ruleBase)):
             rule = self.ruleBase[i]
             produc = rule.compatibility(example)
             produc *= rule.weight
-            if (rule.clas > len(class_degrees) - 1) :
-                aux = [ 0.0 for x in range (len(class_degrees))]
-                for j in range( 0, len(aux)):
+            if produc > max_value:
+                max_value = produc
+                class_value = rule.clas
+        return class_value
+
+    # * Additive Combination FRM
+    # * @param example double[] the input example
+    # * @return int the class label for the set of rules with the highest sum of membership degree per class
+
+    def FRM_AC(self, example):
+        class_value = -1
+        class_degrees = []
+        for i in range(0, len(self.ruleBase)):
+            rule = self.ruleBase[i]
+            produc = rule.compatibility(example)
+            produc *= rule.weight
+            if rule.clas > (len(class_degrees) - 1):
+                aux = [0.0 for x in range(len(class_degrees))]
+                for j in range(0, len(aux)):
                     aux[j] = class_degrees[j]
 
-                class_degrees = [ 0.0 for x in range (rule.clas+1)]
-                for j in range( 0,len(aux)):
+                class_degrees = [0.0 for x in range(rule.clas + 1)]
+                for j in range(0, len(aux)):
                     class_degrees[j] = aux[j]
 
             class_degrees[rule.clas] += produc
 
-         max = 0.0
-         for l in range( 0,len(class_degrees)):
-            if (class_degrees[l] > max) :
-                max = class_degrees[l]
-                clas = l
+        max_value = 0.0
+        for l in range(0, len(class_degrees)):
+            if class_degrees[l] > max_value:
+                max_value = class_degrees[l]
+                class_value = l
 
-         return clas
-
-
-
-
-
-
-
+        return class_value
